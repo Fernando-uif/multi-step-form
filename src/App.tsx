167dd42307d6c6f669215729a66ce79c8d4d9339
Fernando sosa <fsosat1400@alumno.ipn.mx>
2023-03-25 01:36:53 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et ipsa modi
+        nemo, molestias ea temporibus nihil excepturi quia expedita nesciunt
+        accusantium placeat adipisci mollitia veritatis odio sequi animi optio.
+        Officia?
+      </div>
+    </>
+  );
+}
+
+export default App;
